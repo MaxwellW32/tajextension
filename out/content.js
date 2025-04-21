59b8517fa684b1be49efcf1e10489c9313d505ea
MaxwellW32 <maxwellwedderburn32@gmail.com>
2025-04-21 00:49:12 -0500

    latest

--- out/content.js
+++ out/content.js
@@ -1,82 +1,168 @@
 "use strict";
 //Features: 
-//to do
-function handleSearch() {
+//watch the rais stream
+//give reports of errors
+//possibly send emails
+function handleLoad() {
     //wait till page loads
     window.addEventListener("load", () => {
         //give some time after that to ensure js loads
-        setTimeout(() => {
-            var _a;
-            const keywords = ["stopped"];
-            const seenTableHead = document.querySelector("thead");
-            if (seenTableHead === null)
-                return;
-            const titleObj = {};
-            //get the element holding the column titles
-            let titlesTableRow = (_a = seenTableHead.querySelector(".TableHeaderRow")) !== null && _a !== void 0 ? _a : seenTableHead.children[1];
-            if (titlesTableRow === null || titlesTableRow === undefined) {
-                return;
-            }
-            const thChildThNodes = titlesTableRow.childNodes;
-            if (thChildThNodes.length === 0)
-                return;
-            //loop over each td - get the column names
-            let eachThIndex = -1;
-            thChildThNodes.forEach((eachTh) => {
-                //ensure only valid th
-                if (eachTh.tagName !== "TH")
-                    return;
-                eachThIndex++;
-                //get font element
-                const seenAElement = eachTh.querySelector("a");
-                if (seenAElement === null)
-                    return;
-                const seenText = seenAElement.innerText;
-                //add onto object
-                titleObj[eachThIndex] = seenText.toLowerCase();
-            });
-            //check if status seen in titles
-            const seeingStatusHeadingIndex = Object.values(titleObj).findIndex(eachTitleName => eachTitleName === "status");
-            if (seeingStatusHeadingIndex == -1) {
-                console.log(`$not seeing status heading`);
-                return;
-            }
-            //look at data in the table
-            const seenTableBody = document.querySelector("tbody");
-            if (seenTableBody === null)
-                return;
-            const childTrNodes = seenTableBody.childNodes;
-            if (childTrNodes.length === 0)
-                return;
-            let seeingError = false;
-            //loop over each td - get the column values
-            childTrNodes.forEach(eachTR => {
-                //ensure only valid tr
-                if (eachTR.tagName !== "TR")
-                    return;
-                //loop over each td in the row
-                const trChildTdNodes = eachTR.childNodes;
-                if (trChildTdNodes.length === 0)
-                    return;
-                //get the status data for each in the table 
-                let eachTDIndex = -1;
-                trChildTdNodes.forEach((eachTD) => {
-                    if (eachTD.tagName !== "TD")
-                        return;
-                    eachTDIndex++;
-                    if (eachTDIndex === seeingStatusHeadingIndex) {
-                        //check the data in each TD
-                        const seenValue = eachTD.innerText.toLowerCase();
-                        if (keywords.includes(seenValue)) {
-                            seeingError = true;
-                        }
-                    }
-                });
-            });
-            if (seeingError) {
-                chrome.runtime.sendMessage({ type: "alert" });
-            }
-        }, 2000);
+        setTimeout(main, 2000);
     });
 }
-handleSearch();
+handleLoad();
+function main() {
+    let mainObj = {};
+    //check if on login screen
+    const loggedIn = checkIfLoggedIn();
+    if (!loggedIn) {
+        return;
+    }
+    //check for stored main Obj
+    const seenMainObjInStorageStrValue = localStorage.getItem("mainObjStorage");
+    if (seenMainObjInStorageStrValue !== null) {
+        const seenMainObjInStorage = JSON.parse(seenMainObjInStorageStrValue);
+        const seenTimeWritten = new Date(seenMainObjInStorage.timeWritten);
+        const timeDifference = Date.now() - seenTimeWritten.getTime();
+        const canUse = timeDifference <= (1000 * 60 * 10); //10 minutes
+        //if written less than 10 minutes ago - use it
+        if (canUse) {
+            mainObj = seenMainObjInStorage.seenMainObj;
+        }
+        else {
+            //clear item from storage
+            localStorage.removeItem("mainObjStorage");
+        }
+    }
+    const errorKeywords = ["stopped"];
+    //get the column names - Scheduled Start, Status...etc
+    const seenTableHead = document.querySelector("thead");
+    if (seenTableHead === null)
+        return;
+    const titleObj = {};
+    let titlesTableRow = seenTableHead.querySelector(".TableHeaderRow");
+    if (titlesTableRow === null) {
+        return;
+    }
+    const thChildThNodes = titlesTableRow.childNodes;
+    if (thChildThNodes.length === 0)
+        return;
+    //loop over each td - get the column names
+    let eachThIndex = -1;
+    thChildThNodes.forEach((eachTh) => {
+        //ensure only valid th
+        if (eachTh.tagName !== "TH")
+            return;
+        eachThIndex++;
+        //get font element
+        const seenAElement = eachTh.querySelector("a");
+        if (seenAElement === null)
+            return;
+        const seenText = seenAElement.innerText;
+        //add onto object
+        titleObj[eachThIndex] = seenText.toLowerCase();
+    });
+    //check if status and scheduled start seen in column titles
+    const statusHeadingIndex = Object.values(titleObj).findIndex(eachTitleName => eachTitleName === "status");
+    const scheduledStartHeadingIndex = Object.values(titleObj).findIndex(eachTitleName => eachTitleName === "scheduled start");
+    if (statusHeadingIndex == -1 || scheduledStartHeadingIndex === -1) {
+        console.log(`$not seeing required info - statusHeadingIndex/scheduledStartHeadingIndex`);
+        return;
+    }
+    //get the data in the table
+    const seenTableBody = document.querySelector("tbody");
+    if (seenTableBody === null)
+        return;
+    const childTrNodes = seenTableBody.childNodes;
+    if (childTrNodes.length === 0)
+        return;
+    //loop over each td - get the column values
+    childTrNodes.forEach(eachTR => {
+        //ensure only valid tr
+        if (eachTR.tagName !== "TR")
+            return;
+        //loop over each td in the row
+        const trChildTdNodes = eachTR.childNodes;
+        if (trChildTdNodes.length === 0)
+            return;
+        //get the status data for each in the table 
+        let seenStatusText = "";
+        let seenScheduledStartText = "";
+        let eachTDIndex = -1;
+        trChildTdNodes.forEach((eachTD) => {
+            if (eachTD.tagName !== "TD")
+                return;
+            eachTDIndex++;
+            const seenValue = eachTD.innerText.toLowerCase();
+            if (eachTDIndex === statusHeadingIndex) {
+                //assign status
+                seenStatusText = seenValue;
+            }
+            else if (eachTDIndex === scheduledStartHeadingIndex) {
+                //assign scheduled start
+                seenScheduledStartText = seenValue;
+            }
+        });
+        //ensure text info seen
+        if (seenStatusText === "" || seenScheduledStartText === "") {
+            console.log(`not seeing text info`);
+            return;
+        }
+        //assign object data for each row
+        //start off new
+        if (mainObj[seenScheduledStartText] === undefined) {
+            mainObj[seenScheduledStartText] = {
+                status: seenStatusText,
+            };
+        }
+        //update records if they exist already
+        mainObj[seenScheduledStartText].status = seenStatusText;
+        //assign errors
+        if (errorKeywords.includes(mainObj[seenScheduledStartText].status)) {
+            //create new if doesnt exist
+            if (mainObj[seenScheduledStartText].error === undefined) {
+                mainObj[seenScheduledStartText].error = {
+                    errorText: "rais stream stopped",
+                    acknowledged: false
+                };
+            }
+            else {
+                //update records if exists already
+                mainObj[seenScheduledStartText].error.errorText = "rais stream stopped";
+            }
+        }
+    });
+    console.log(`mainObj`, mainObj);
+    let runningDetected = false;
+    Object.entries(mainObj).map(eachEntry => {
+        const eachKey = eachEntry[0];
+        const eachValue = eachEntry[1];
+        if (eachValue.error !== undefined && !eachValue.error.acknowledged) {
+            if (mainObj[eachKey].error === undefined)
+                return;
+            chrome.runtime.sendMessage({ type: "errorAlert" });
+            mainObj[eachKey].error.acknowledged = true;
+        }
+        //check the status in mainObj
+        if (eachValue.status === "running") {
+            runningDetected = true;
+        }
+    });
+    //if nothing running stop the loop
+    if (!runningDetected) {
+        chrome.runtime.sendMessage({ type: "stopMonitoring" });
+    }
+    //update object in storage
+    const newMainObjInStorage = {
+        seenMainObj: mainObj,
+        timeWritten: new Date()
+    };
+    //write to storage
+    localStorage.setItem("mainObjStorage", JSON.stringify(newMainObjInStorage));
+}
+function checkIfLoggedIn() {
+    let loggedIn = true;
+    //scan html
+    //if login screen detected focus window
+    return loggedIn;
+}
--- src/background.ts
+++ src/background.ts
@@ -1,70 +1,93 @@
 const TARGET_URL = "https://gentax-external.ads.taj.gov.jm/Monitoring";
-const KEYWORDS = ["stopped"];
 const MONITOR_TIMES = [17, 20]; // 5pm and 8pm in 24-hr
 
+let programMonitoring = false
+
+//edit to do
+//turn on stop running
+
 chrome.runtime.onInstalled.addListener(() => {
-    // Setup hourly check
-    chrome.alarms.create("monitorStreams", {
-        periodInMinutes: 5
-    });
+    // start at appropriate time - every 10 minutes check if we can start
+    if (!programMonitoring) {
+        chrome.alarms.create("starter", {
+            periodInMinutes: 5
+        });
+    }
 });
 
-chrome.alarms.onAlarm.addListener((alarm) => {
-    if (alarm.name === "monitorStreams") {
+chrome.alarms.onAlarm.addListener(async (alarm) => {
+    //run starter only if havent started
+    if (alarm.name === "starter" && !programMonitoring) {
+        console.log(`running starter`)
+
         const now = new Date();
         const hour = now.getHours();
 
         if (MONITOR_TIMES.includes(hour)) {
+            programMonitoring = true
+
+            //clear previous alarm
+            await chrome.alarms.clear("monitorStreams")
+
+            //start 5 minute checker
+            chrome.alarms.create("monitorStreams", {
+                periodInMinutes: 1
+            });
+
+            //start once initially
             monitorStreamPage();
         }
     }
-});
 
-let alertTabId: number | null = null;
+    if (alarm.name === "monitorStreams") {
+        console.log(`running monitorStreams`)
 
-chrome.runtime.onMessage.addListener((msg) => {
-    if (msg.type === "alert") {
-        const alertUrl = chrome.runtime.getURL("alert.html");
-
-        if (alertTabId !== null) {
-            // Try to reuse existing tab
-            chrome.tabs.update(alertTabId, { active: true });
-
-        } else {
-            // Open new tab
-            chrome.tabs.create({ url: alertUrl, active: true }, (tab) => {
-                alertTabId = tab.id || null;
-            });
-        }
+        monitorStreamPage();
     }
 });
 
-// Clear saved tab ID if the tab is closed
-chrome.tabs.onRemoved.addListener((tabId) => {
-    if (tabId === alertTabId) {
-        alertTabId = null;
+chrome.runtime.onMessage.addListener((msg) => {
+    if (msg.type === "errorAlert") {
+        const alertUrl = chrome.runtime.getURL("alert.html");
+
+        // Open new tab
+        chrome.tabs.create({ url: alertUrl, active: true }, (tab) => {
+        });
+    }
+
+    //stop monitoring
+    if (msg.type === "stopMonitoring") {
+        console.log(`monitoring stopped`)
+
+        chrome.alarms.clear("monitorStreams")
+
+        programMonitoring = false
     }
 });
 
 async function monitorStreamPage() {
     const tabs: chrome.tabs.Tab[] = await chrome.tabs.query({ url: `${TARGET_URL}/*` });
 
+    //ensure taj tab exists
     if (tabs.length === 0) {
         chrome.tabs.create({ url: TARGET_URL });
 
-        return;
+        //run again
+        monitorStreamPage()
+        return
     }
 
     const tab = tabs[0];
 
     if (tab.id === undefined) {
+        console.log(`tab id undefined`, tab)
         return
     }
 
-    // Refresh tab to keep session alive
+    // reload tab
     chrome.tabs.reload(tab.id);
 
-    // Inject content script to check for keyword
+    // execute script
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
         files: ["out/content.js"]
